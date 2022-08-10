639bc34ab83c47ab6c6cc6c14bb36a7b25087c75
박진주 <89890161+blockodyssey-jjpark@users.noreply.github.com>
2022-08-10 19:57:57 +0900

    chore: 필수 라이브러리 셋팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider } from 'jotai';
+
+import '../styles/globals.css';
+
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
